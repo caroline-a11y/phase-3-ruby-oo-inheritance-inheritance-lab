00bf885be8f32cc964d05df7f97da559cf39f577
Caroline Syokau <caroline.syokau@student.moringaschool.com>
2023-02-19 21:45:26 +0300

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
 class Student 
 
 end
+class Student < user
+    def initialize
+        @knowledge = []
+    end
+    def learn (student_details)
+        @knowledge << student_details
+    end
+    def knowledge
+        @knowledge
+    end 
+end
